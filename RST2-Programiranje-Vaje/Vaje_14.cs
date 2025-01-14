f3dffa2778019f4a9257372b1bc9af88ef50f487
borutluzar <borut.luzar@gmail.com>
2025-01-15 00:08:43 +0100

    Rešitve 14. vaj

--- RST2-Programiranje-Vaje/Vaje_14.cs
+++ RST2-Programiranje-Vaje/Vaje_14.cs
@@ -1,4 +1,7 @@
-﻿namespace RST2_Programiranje_Vaje
+﻿using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RST2_Programiranje_Vaje
 {
     public enum Vaje_14_Naloge
     {
@@ -21,16 +24,75 @@
         /// </summary>
         public static void Naloga731()
         {
-            
+            int numLists = 1000;
+            int numElements = 20_000;
+            int intervalSize = 100_000;
+
+            List<List<int>> lstAllLists = new List<List<int>>();
+            Random rnd = new Random();
+
+            // Pripravimo sezname
+            for (int i = 0; i < numLists; i++)
+            {
+                lstAllLists.Add(new List<int>());
+                for (int j = 0; j < numElements; j++)
+                {
+                    lstAllLists[i].Add(rnd.Next(1, intervalSize + 1));
+                }
+            }
+
+            // Poiščimo seznam z maksimalnim številom praštevil 
+            // Zaporedno
+            Stopwatch sw = Stopwatch.StartNew();
+            int maxSerial = lstAllLists.Max(x => x.Count(IsPrime));
+            Console.WriteLine($"Največje število praštevil v enem seznamu je: {maxSerial}, " +
+                $"izračunano zaporedno v {sw.Elapsed.TotalSeconds:0.00} sekundah.");
+
+            // Paralelno - nad seznami samo pokličemo funkcijo AsParallel!
+            sw.Restart();
+            int maxParallel = lstAllLists.AsParallel().Max(x => x.Count(IsPrime));
+            Console.WriteLine($"Največje število praštevil v enem seznamu je: {maxParallel}, " +
+                $"izračunano paralelno v {sw.Elapsed.TotalSeconds:0.00} sekundah.");
         }
 
         /// <summary>
-        /// Napišite funkcijo, ki poišče najmanjše število z natanko 100 delitelji. 
+        /// Napišite funkcijo, ki poišče najmanjše število z natanko 200 delitelji. 
         /// Implementirajte jo zaporedno in vzporedno in primerjajte čas izvajanja.
         /// </summary>
         public static void Naloga732()
         {
+            int listSize = int.MaxValue; //1_000_000; //10_000_000;
+            int numDivs = 200;
 
+            var lstNums = Enumerable.Range(1, listSize);
+
+            // Zaporedno
+            Stopwatch sw = Stopwatch.StartNew();
+            // First najde prvi element in zaključi funkcijo
+            int minSerial = lstNums.First(x => NumDivisors(x) == numDivs);
+
+            /* Ali ekvivalentno:
+            int minSerial = 0;
+            foreach (int num in lstNums)
+            {
+                if(NumDivisors(num) == numDivs)
+                {
+                    minSerial = num;
+                    break;
+                }
+            }
+            */
+            Console.WriteLine($"Zaporedno: prvo število s {numDivs} delitelji je {minSerial}. Čas: {sw.Elapsed.TotalSeconds:0.00}");
+
+            // Paralelno
+            sw.Restart();
+
+            // Uporaba == je bistveno počasnejša od >=, razloga še ne poznam
+            // Sta pa v primerih numDivs = 100 in numDivs = 200 rezultata enaka, ampak v splošnem to ne bo nujno res.
+            //int minParallel = lstNums.AsParallel().AsOrdered().First(x => NumDivisors(x) == numDivs);
+            int minParallel = lstNums.AsParallel().AsOrdered().First(x => NumDivisors(x) >= numDivs);
+            Console.WriteLine($"Paralelno: prvo število s {numDivs} delitelji je {minParallel}. " +
+                    $"Čas: {sw.Elapsed.TotalSeconds:0.00}");
         }
 
         /// <summary>
@@ -45,7 +107,34 @@
         /// </summary>
         public static void Naloga741()
         {
-
+            // Nismo reševali.
         }
-    }    
+
+        public static bool IsPrime(int i)
+        {
+            for (int j = 2; j <= (int)Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public static int NumDivisors(int n)
+        {
+            int count = 0;
+            int sqrt = (int)Math.Sqrt(n);
+
+            for (int i = 1; i <= sqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                    if (i != n / i) // Prištejemo še deliteljev par 
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
 }
